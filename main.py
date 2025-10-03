c0f41a7ce63c3c1cde335f376a9be281470f2b69
adrianmarino19 <adrianmarino10@gmail.com>
2025-10-03 23:15:49 +0200

    first step search agent

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+from langchain import hub
+from langchain.agents import AgentExecutor
+from langchain.agents.react.agent import create_react_agent
+
+load_dotenv()
+
+
+
+###
+
+def main():
+    print('love you')
+
+
+if __name__ == "__main__":
+    main()
