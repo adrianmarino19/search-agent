8f6cf9a1274d385b7780d4a2261701b43f496de7
adrianmarino19 <adrianmarino10@gmail.com>
2025-10-04 19:28:56 +0200

    decorators

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv # this is simply to be able to load the environment, but you still need to us os!
 
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+from langchain_core.tools import tool # tool decorator which is how we pass tools to LLMs!
 
 load_dotenv()
 
 
+tool = [TavilySearch]
 
 ###
 
 def main():
-    print('love you')
+    print()
 
 
 if __name__ == "__main__":
